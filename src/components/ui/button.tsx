838b330a709927e7d5e88b33cd6c6668baf5862b
T4N <t4nt3ch@gmail.com>
2026-07-01 16:00:31 +0100

    fix: post-build fix - src/components/ui/button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,16 +15,14 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <button
         className={cn(
           "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-          {
-            "bg-primary text-primary-foreground hover:bg-primary/90": variant === "default",
-            "border border-input bg-background hover:bg-accent hover:text-accent-foreground": variant === "outline",
-            "hover:bg-accent hover:text-accent-foreground": variant === "ghost",
-          },
-          {
-            "h-10 px-4 py-2": size === "default",
-            "h-9 rounded-md px-3": size === "sm",
-            "h-11 rounded-md px-8": size === "lg",
-          },
+          variant === "default" &&
+            "bg-primary text-primary-foreground hover:bg-primary/90",
+          variant === "outline" &&
+            "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          variant === "ghost" && "hover:bg-accent hover:text-accent-foreground",
+          size === "default" && "h-10 px-4 py-2",
+          size === "sm" && "h-9 rounded-md px-3",
+          size === "lg" && "h-11 rounded-md px-8",
           className
         )}
         ref={ref}
