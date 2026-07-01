8bd837364846ab42527afd83bab2202fb9795677
T4N <t4nt3ch@gmail.com>
2026-07-01 16:55:39 +0100

    fix: post-build fix - src/components/ui/use-toast.tsx

--- src/components/ui/use-toast.tsx
+++ src/components/ui/use-toast.tsx
@@ -19,6 +19,8 @@ type ToasterToast = ToastProps & {
   title?: React.ReactNode;
   description?: React.ReactNode;
   action?: ToastActionElement;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
 };
 
 const actionTypes = {
