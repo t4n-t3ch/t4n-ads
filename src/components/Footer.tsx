6c27127992262a6723f6eb8eb6789557eae3276d
T4N <t4nt3ch@gmail.com>
2026-07-02 11:28:03 +0100

    fix: post-build fix - src/components/Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -122,7 +122,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center space-x-4">
                 {navigation.social.map((item) => (
-                  <Link
+                  <a
                     key={item.name}
                     href={item.href}
                     target="_blank"
@@ -131,7 +131,7 @@ const Footer = () => {
                     aria-label={item.name}
                   >
                     {item.label}
-                  </Link>
+                  </a>
                 ))}
               </div>
             </div>
